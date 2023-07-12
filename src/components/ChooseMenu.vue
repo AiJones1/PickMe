3c47bc55996ae69906f9fbb5e80b25cd15c2f706
Aidan <amjcentre@gmail.com>
2023-07-12 21:40:48 +1000

    Refactor Code into Components

---
+++ src/components/ChooseMenu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Picking</h1>
+    <v-select
+      v-model="selectionView"
+      label="Select"
+      :items="items"
+      item-title="title"
+      item-value="value"
+      variant="solo-filled"
+    ></v-select>
+
+    <v-btn v-if="selectionView === 'dinner'" @click="pickDinner()"
+      >Choose Dinner</v-btn
+    >
+    <v-btn v-if="selectionView === 'date'" @click="pickDate()"
+      >Choose Date</v-btn
+    >
+
+    <h3>Tonight's Dinner: {{ tonightDinner }}</h3>
+    <h3>Tonight's date: {{ tonightOption }}</h3>
+  </div>
+</template>
+<script setup>
+import {ref} from "vue";
+const dinner = ["chicken", "death", "kebabs"];
+const date = ["movie", "restaurant", "arcade"];
+let tonightDinner = ref(" ");
+let tonightOption = ref(" ");
+const items = [
+  { title: "Dinner", value: "dinner" },
+  { title: "Date", value: "date" },
+];
+let selectionView = "dinner";
+
+function pickDinner() {
+  tonightDinner.value = dinner[Math.floor(Math.random() * dinner.length)];
+  console.log(dinner.length);
+  console.log(tonightDinner.value);
+}
+
+function pickDate() {
+  tonightOption.value = date[Math.floor(Math.random() * date.length)];
+  console.log(date.length);
+  console.log(tonightOption.value);
+}
+</script>
