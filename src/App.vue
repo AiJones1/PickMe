9ceae26a7e33ee749e0e7234d87359e6b6c5db43
Alejandro Munoz <alex@digitalmaas.com>
2023-07-13 20:32:23 +1000

    made some styling changes to the website

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <title>Pick 4 Me</title>
-  <h1>
-    Pick 4 Me
-    <Icon
-      icon="fluent-emoji-flat:wheel"
-      width="44"
-      height="44"
-    />
-  </h1>
-  <main>
-    <picking />
-  </main>
+  <v-container>
+    <v-row
+      class="flex justify-center"
+    >
+      <h1>Pick 4 Me</h1>
+      <Icon
+        icon="fluent-emoji-flat:wheel"
+        width="44"
+        height="44"
+      />
+    </v-row>
+    <v-row justify="center">
+      <picking />
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
@@ -20,7 +23,6 @@ import { Icon } from '@iconify/vue'
 
 <style scoped>
 h1 {
-  margin: 0 auto;
   color: rgb(85, 4, 85);
   font-size: 40px;
   font-display: block;
--- src/components/ChooseMenu.vue
+++ src/components/ChooseMenu.vue
@@ -1,118 +1,122 @@
 <template>
-  <div>
-    <h2>Picking</h2>
-    <v-select
-      v-model="selectionView"
-      label="Select"
-      :items="items"
-      item-title="title"
-      item-value="value"
-      variant="solo-filled"
-    />
+  <div class="pt-8">
+    <v-col cols="12">
+      <v-card>
+        <div class="py-4 px-4">
+          <h2 class="pb-8">
+            Picking
+          </h2>
+          <v-select
+            v-model="selectionView"
+            label="Select"
+            :items="items"
+            item-title="title"
+            item-value="value"
+            variant="solo-filled"
+          />
 
-    <v-btn
-      v-if="selectionView === 'dinner'"
-      @click="pickDinner()"
-    >
-      Choose Dinner
-    </v-btn>
-    <v-btn
-      v-else-if="selectionView === 'date'"
-      @click="pickDate()"
-    >
-      Choose Date
-    </v-btn>
-    <v-select
-      v-model="seeMenu"
-      label="See Choices"
-      :items="opts"
-      item-title="title"
-      item-value="value"
-      variant="solo-filled"
-    />
+          <v-btn
+            v-if="selectionView === 'dinner'"
+            class="py-4"
+            @click="pickDinner()"
+          >
+            Choose Dinner
+          </v-btn>
+          <v-btn
+            v-else
+            class="py-4"
+            @click="pickDate()"
+          >
+            Choose Date
+          </v-btn>
+          <v-select
+            v-model="seeMenu"
+            class="py-4"
+            label="See Choices"
+            :items="opts"
+            item-title="title"
+            item-value="value"
+            variant="solo-filled"
+          />
 
-    <h3 v-if="selectionView === 'dinner'">
-      Tonight's Dinner: {{ tonightDinner }}
-    </h3>
-    <h3 v-else-if="selectionView === 'date'">
-      Tonight's date: {{ tonightOption }}
-    </h3>
+          <h3 v-if="selectionView === 'dinner'">
+            Tonight's Dinner: {{ tonightDinner }}
+          </h3>
+          <h3 v-else>
+            Tonight's date: {{ tonightOption }}
+          </h3>
 
-    Dinner Options
-    <ul
-      v-if="selectionView === 'dinner'"
-      id="dinnerOptions"
-    >
-      <li
-        v-for="value in dinner"
-        v-if="seeMenu ==='yes'"
+        <!-- <h2>Dinner Options</h2>
+    <v-list v-if="seeMenu">
+      <v-list-item
+        v-for="dinner in dinnerIdeas"
+        :key="dinner"
       >
-        {{ value }}
-      </li>
-    </ul>
+        {{ dinner }}
+      </v-list-item>
+    </v-list>
     <h3>Date Options</h3>
-    <ul
-      v-if="selectionView === 'date'"
-      id="dateOptions"
-    >
-      <li
-        v-for="value in date"
-        v-if="seeMenu ==='yes'"
-        :key="value"
+    <v-list v-if="seeMenu">
+      <v-list-item
+        v-for="date in dateIdeas"
+        :key="date"
       >
-        {{ value }}
-      </li>
-    </ul>
+        {{ date }}
+      </v-list-item>
+    </v-list> -->
+        </div>
+      </v-card>
+    </v-col>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
 
 const dinner = [
-  "Chicken Katsu Curry",
-  "Frozen Pizza",
-  "Kebabs",
-  "Hot Pot",
-  "Dominos",
-  "Greek Takeout",
-  "Chinese Food",
-  "KFC",
-  "Pork Belly",
-  "Pad Thai Chicken",
-  "Burgers",
-  "Salmon and Cous Cous",
-  "HSP",
-];
-const date = [
-  "Cinemas",
-  "Restaurant",
-  "Arcade",
-  "Beach Picnic",
-  "Bottomless Brunch",
-  "Movie Drinking Game",
-  "Bar hopping",
-  "Spa night",
-];
-let tonightDinner = ref(" ");
-let tonightOption = ref(" ");
+  'Chicken Katsu Curry',
+  'Frozen Pizza',
+  'Kebabs',
+  'Hot Pot',
+  'Dominos',
+  'Greek Takeout',
+  'Chinese Food',
+  'KFC',
+  'Pork Belly',
+  'Pad Thai Chicken',
+  'Burgers',
+  'Salmon and Cous Cous',
+  'HSP',
+]
+const dateIdeas = ref([
+  'Cinemas',
+  'Restaurant',
+  'Arcade',
+  'Beach Picnic',
+  'Bottomless Brunch',
+  'Movie Drinking Game',
+  'Bar hopping',
+  'Spa night',
+])
+let tonightDinner = ref(' ')
+let tonightOption = ref(' ')
 const items = [
-  { title: "Dinner", value: "dinner" },
-  { title: "Date", value: "date" },
-];
-let selectionView = "dinner";
-let seeMenu = "no";
+  { title: 'Dinner', value: 'dinner' },
+  { title: 'Date', value: 'date' },
+]
+let selectionView = ref('dinner')
+let seeMenu = ref(false)
 
-const opts =[
-  {title: "Yes", value:"yes"},
-  {title: "No", value: "no" }
+const opts = [
+  { title: 'Yes', value: true },
+  { title: 'No', value: false },
 ]
 
 function pickDinner() {
-  tonightDinner.value = dinner[Math.floor(Math.random() * dinner.length)];
+  tonightDinner.value = dinner[Math.floor(Math.random() * dinner.length)]
 }
 
 function pickDate() {
-  tonightOption.value = date[Math.floor(Math.random() * date.length)];
+  tonightOption.value = date[Math.floor(Math.random() * date.length)]
 }
 
 // function dinnerMenu() {
@@ -135,4 +139,5 @@ h3 {
   color: rgb(9, 39, 102);
   font-size: 18px;
 }
+
 </style>
